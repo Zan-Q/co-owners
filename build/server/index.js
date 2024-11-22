63f8d7fe5075687588d55ba835d982f3ec0e1356
Peter <peter.tang.lai@gmail.com>
2024-11-22 10:06:05 -0700

    link corrections

--- build/server/index.js
+++ build/server/index.js
@@ -1058,7 +1058,7 @@ class GoogleProvider {
       {
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: "http://localhost:3000/auth/google/callback",
+        redirect_uri: "https://co-owners.ca/auth/google/callback",
         authorizationParams: {
           scope: ["openid", "email"]
         },
