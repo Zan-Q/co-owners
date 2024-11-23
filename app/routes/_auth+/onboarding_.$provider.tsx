96d1ac0ee8e698121df7292fa6f97f5316574e56
Peter <peter.tang.lai@gmail.com>
2024-11-23 13:37:08 -0700

    Onboard dark mode
    Fix Google onboard title dark mode

--- app/routes/_auth+/onboarding_.$provider.tsx
+++ app/routes/_auth+/onboarding_.$provider.tsx
@@ -227,7 +227,7 @@ export default function OnboardingProviderRoute() {
 		<div className="container flex min-h-full flex-col justify-center pb-32 pt-20">
 			<div className="mx-auto w-full max-w-lg">
 				<div className="flex flex-col gap-3 text-center">
-					<h1 className="text-h1">Welcome aboard {data.email}!</h1>
+					<h1 className={`text-h1 ${prefersDarkMode ? 'text-white' : 'text-black'}`}>Welcome aboard {data.email}!</h1>
 					<p className={`text-body-md text-muted-foreground ${prefersDarkMode ? 'text-white' : 'text-black'}`}>
 						Please enter your details.
 					</p>
